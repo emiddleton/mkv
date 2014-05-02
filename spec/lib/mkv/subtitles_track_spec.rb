0c9a3779194c8126ffd2420d61d01118e60bef77
Eric Collins <tabfugnic@gmail.com>
2014-05-02 18:00:46 -0400

    Formatting

--- spec/lib/mkv/subtitles_track_spec.rb
+++ spec/lib/mkv/subtitles_track_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-SUBTITLES_TRACK =  {track_type: 'subtitles', codec_id: 'A_AAC', sampling_frequency: '48000', channels: '2', language: 'eng', track_number: '4', track_uid: '1231234', lacing_flag: '0', language: 'eng', enabled: '0', default_flag: '0', forced_flag: '0' }
+SUBTITLES_TRACK =  { track_type: 'subtitles', codec_id: 'A_AAC', sampling_frequency: '48000', channels: '2', language: 'eng', track_number: '4', track_uid: '1231234', lacing_flag: '0', language: 'eng', enabled: '0' }
 
 describe MKV::SubtitlesTrack, '#type' do
   it 'returns the type' do
